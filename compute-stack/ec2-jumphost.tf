34c16601110042bf389b85586f8342739749c79c
KGK <k.kafalian@gmail.com>
2022-08-28 11:50:08 +0400

    fixed

--- compute-stack/ec2-jumphost.tf
+++ compute-stack/ec2-jumphost.tf
@@ -68,17 +68,18 @@ resource "aws_instance" "data-ec2-jumphost" {
 
   provisioner "remote-exec" {
     inline = [
-        "sudo echo -e "[all-hosts]" >> /etc/ansible/hosts",
-        "sudo echo -e "worker01 ansible_host=192.168.2.106" >> /etc/ansible/hosts",
-        "sudo echo -e "worker02 ansible_host=192.168.2.197" >> /etc/ansible/hosts",
-        "sudo echo -e "master01 ansible_host=192.168.2.130" >> /etc/ansible/hosts",
-        "sudo echo -e "[localhost]" >> /etc/ansible/hosts",
-        "sudo echo -e "127.0.0.1" >> /etc/ansible/hosts",
-        "sudo echo -e "[worker]" >> /etc/ansible/hosts",
-        "sudo echo -e "worker01 ansible_host=192.168.2.106" >> /etc/ansible/hosts",
-        "sudo echo -e "worker02 ansible_host=192.168.2.197" >> /etc/ansible/hosts",
-        "sudo echo -e "[master]" >> /etc/ansible/hosts",
-        "sudo echo -e "master01 ansible_host=192.168.2.130" >> /etc/ansible/hosts",
+        "sudo sleep 2", 
+        "sudo echo -e '[all-hosts]' >> /etc/ansible/hosts",
+        "sudo echo -e 'worker01 ansible_host=192.168.2.106' >> /etc/ansible/hosts",
+        "sudo echo -e 'worker02 ansible_host=192.168.2.197' >> /etc/ansible/hosts",
+        "sudo echo -e 'master01 ansible_host=192.168.2.130' >> /etc/ansible/hosts",
+        "sudo echo -e '[localhost]' >> /etc/ansible/hosts",
+        "sudo echo -e '127.0.0.1' >> /etc/ansible/hosts",
+        "sudo echo -e '[worker]' >> /etc/ansible/hosts",
+        "sudo echo -e 'worker01 ansible_host=192.168.2.106' >> /etc/ansible/hosts",
+        "sudo echo -e 'worker02 ansible_host=192.168.2.197' >> /etc/ansible/hosts",
+        "sudo echo -e '[master]' >> /etc/ansible/hosts",
+        "sudo echo -e 'master01 ansible_host=192.168.2.130' >> /etc/ansible/hosts",
     ]
   }
 
